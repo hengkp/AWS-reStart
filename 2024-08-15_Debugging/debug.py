6b7a8b1f0e4bcc5274e06c7eb8b7ad005be5b7ae
EC2 Default User <kriengkrai.pho@gmail.com>
2024-08-15 13:09:42 +0000

    Update 15-08-2024

---
+++ 2024-08-15_Debugging/debug.py
@@ -0,0 +1,3 @@
+
+age = int(input("Please write your age: "))
+assert age > 0, "Invalid age was supplied."
---
+++ lab15_os_subprocess.py
@@ -0,0 +1,27 @@
+import os
+
+# os.system("ls")
+
+
+# subprocess.run(args, *, stdin=None, input=None, stdout=None, stderr=None, capture_output=False, shell=False, cwd=None, timeout=None, check=False, encoding=None, errors=None, text=None, env=None, universal_newlines=None)
+import subprocess
+
+# subprocess.run(["ls"])
+# subprocess.run(["ls","-l"])
+# subprocess.run(["ls","-l","README.md"])
+
+
+# command="uname"
+# commandArgument="-a"
+# print(f'Gathering system information with command: {command} {commandArgument}')
+# subprocess.run([command,commandArgument])
+
+command="ps"
+commandArgument="-x"
+print(f'Gathering active process information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
+
+# output = subprocess.run([command,commandArgument], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# print(output)
+
+
